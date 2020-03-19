b4e917f5386c6f8c3efc01bfe895999973eb680a
Khokholeva <svetahoholeva759@gmail.com>
2020-03-19 20:17:10 +0500

    still start

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextA
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 
-
 User = __all_models.users.User
 News = __all_models.news.News
 
@@ -26,7 +25,6 @@ class RegisterForm(FlaskForm):
 def main():
     db_session.global_init("db/blogs.sqlite")
     session = db_session.create_session()
-    user = session.query(User).filter(User.id == 1).first()
     app.run()
 
 
@@ -35,6 +33,31 @@ def index():
     session = db_session.create_session()
     news = session.query(News).filter(News.is_private != True)
     return render_template("index.html", news=news)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
