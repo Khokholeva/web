d45f09ce614d1a5565e3986b1e805a15f8135ba1
777 <svetahoholeva759@gmail.com>
2020-04-09 19:56:47 +0500

    api 3

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 from requests import get
 
-print(get('http://localhost:5000/api/jobs/g').json())
+print(get('http://localhost:5000/api/jobs').json())
+print(get('http://localhost:5000/api/jobs/1').json())
+print(get('http://localhost:5000/api/jobs/11').json())
+print(get('http://localhost:5000/api/jobs/ff').json())
